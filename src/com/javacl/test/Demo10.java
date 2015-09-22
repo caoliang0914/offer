5e1e749a345e2f461a04d973b01017e47dc37057
caoliang0914 <caoliang0914@gmail.com>
2015-09-22 17:11:16 +0800

    斐波那契数列非递归算法

--- src/com/javacl/test/Demo10.java
+++ src/com/javacl/test/Demo10.java
@@ -8,7 +8,7 @@ public class Demo10 {
     public static void main(String[] args) {
         System.out.println("9的二进制包含1的个数为：" + numberOf1(9));
     }
-
+               
     public static int numberOf1 (int n) {
        int count = 0;
        while (n !=0) {
