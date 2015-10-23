195adba74717dae7e672767a88313104c95e0114
caoliang0914 <caoliang0914@gmail.com>
2015-10-23 14:46:02 +0800

    修复bug

--- src/com/javacl/test/util/ComplexListNode.java
+++ src/com/javacl/test/util/ComplexListNode.java
@@ -3,5 +3,5 @@ package com.javacl.test.util;
 public class ComplexListNode {
 	public int m_nValue;
 	public ComplexListNode m_pNext;
-	public ComplexListNode m_Sibling;
+	public ComplexListNode m_pSibling;
 }
