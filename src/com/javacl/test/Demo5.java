f8eb18c69585001ec5ce1ac944e7e56c5415bdf6
caol <caoliang0914@gmail.com>
2016-04-05 20:00:26 +0800

    修复项目路径

--- src/com/javacl/test/Demo5.java
+++ src/com/javacl/test/Demo5.java
@@ -1,14 +1,5 @@
 package com.javacl.test;
 
-import com.sun.org.apache.bcel.internal.generic.LNEG;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.Stack;
-
 /**
  * 从尾到头遍历链表
  * Created by caoliang on 2015/9/21.
